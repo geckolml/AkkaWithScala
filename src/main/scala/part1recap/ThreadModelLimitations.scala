9ab890635991e6f3f0d1a96745b78b7d7aeb634a
Renzo <rcerront@uni.pe>
2018-11-29 18:23:16 -0500

    Actors: Intro, Capabilities and Changing Actor Behavior

--- src/main/scala/part1recap/ThreadModelLimitations.scala
+++ src/main/scala/part1recap/ThreadModelLimitations.scala
@@ -1,5 +1,7 @@
 package part1recap
 
+import scala.concurrent.Future
+
 object ThreadModelLimitations extends App {
 
 
@@ -77,5 +79,15 @@ object ThreadModelLimitations extends App {
     * DR #3: tracing and dealing with errors in a multithreaded env is PITN
     */
   // 1M numbers in between 10 threads
-  
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  val futures = (0 to 9)
+    .map(i => 100000 * i until 100000 * (i + 1) ) // 0 - 99999, 100000 - 199999, 200000 - 299999 etc
+    .map(range => Future {
+    if(range.contains(546735)) throw new RuntimeException("invalid number")
+    range.sum
+  })
+
+  val sumFuture = Future.reduceLeft(futures)(_ + _) // Future with the sum of all the numbers
+  sumFuture.onComplete(println)
 }
